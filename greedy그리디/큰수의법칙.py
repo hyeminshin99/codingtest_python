25d16e8ed35d4d6b1b8c51e1f1efe7b267c90557
Ha-moon <hyemin9974@gmail.com>
2021-04-26 23:26:15 +0900

    큰 수의법칙 조건

--- greedy그리디/큰수의법칙.py
+++ greedy그리디/큰수의법칙.py
@@ -1,3 +1,6 @@
 # 주어진 배열의 수들을 M번 더하여, 가장 큰 수 만들기
 # 특정 인덱스의 수가 연속해서 K번 초과하여 더해질 수 없다.
 # ex) 6 + 6 + 6 + 5 + 6 + 6 + 6 + 5  =  46
+
+n, m, k = map(int,input().split())
+data = list(map(int,input().split()))
