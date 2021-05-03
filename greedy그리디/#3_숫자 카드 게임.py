46f67b248aa28cfa180cf306edb9f2f1e1ed0da5
Ha-moon <hyemin9974@gmail.com>
2021-05-03 18:46:55 +0900

    #3_복기 후 수정

--- greedy그리디/#3_숫자 카드 게임.py
+++ greedy그리디/#3_숫자 카드 게임.py
@@ -1,8 +1,12 @@
-# greedy
+# 각 행의 가장 낮은 숫자들 중, 가장 큰 숫자 출력
+
 N, M = map(int, input().split())
-arr = [[int(x) for x in input().split()] for y in range(N)] #2차원배열한번에받기 @외
+#arr = [[int(x) for x in input().split()] for y in range(N)] #2차원배열한번에받기 @외
+                                                            # 저장공간 낭비
 marr=[]
 
-for data in arr:
-  marr.append(min(data))
+for i in range(N): # 1줄씩 받기(2차원배열저장공간 세이브)
+    arr = list(map(int, input().split()))
+    marr.append(min(arr)) # append([E,X]):[E,X]추가 / extend([H,M]):E,X 추가
+
 print(max(marr))
