d9f3eef2c39dfab4200503ae60afb244b3fc7b40
Ha-moon <hyemin9974@gmail.com>
2021-05-03 18:26:42 +0900

    #3_내코드1

--- greedy그리디/#3_숫자 카드 게임.py
+++ greedy그리디/#3_숫자 카드 게임.py
@@ -1,1 +1,8 @@
 # greedy
+N, M = map(int, input().split())
+arr = [[int(x) for x in input().split()] for y in range(N)] #2차원배열한번에받기 @외
+marr=[]
+
+for data in arr:
+  marr.append(min(data))
+print(max(marr))
