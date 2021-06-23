8b2c413914ad7a6b1beced98d8e398a8b29d203e
Ha-moon <hyemin9974@gmail.com>
2021-06-23 21:46:52 +0900

    #2 - 이진탐색

--- 이진탐색/#2_부품찾기.py
+++ 이진탐색/#2_부품찾기.py
@@ -13,3 +13,25 @@ for m in mlist:
     else: ans+='no '
 
 print(ans)
+
+
+#### 이진탐색-반복문 ####
+def BS(array, target, start, end):
+    while start <= end:
+        mid = (start+end)//2
+        if array[mid] == target: return mid
+        elif array[mid] > target: end = mid-1
+        else: start = mid+1
+    return None
+
+N = int(input())
+nlist = list(map(int, input().split()))
+nlist.sort()
+M = int(input())
+mlist = list(map(int, input().split()))
+
+for m in mlist:
+    result = BS(nlist, m, 0, N-1)
+    if result == None:
+        print('no', end=' ')
+    else: print('yes', end=' ')
