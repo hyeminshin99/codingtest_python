dd1913abeb25576508445ac9fd038b612403f4ae
Ha-moon <hyemin9974@gmail.com>
2021-08-27 23:18:31 +0900

    Q44-시간초과 별표

--- 그래프이론/기출문제/Q44_행성터널.py
+++ 그래프이론/기출문제/Q44_행성터널.py
@@ -1,5 +1,5 @@
 # 행성N개 / 각 행성의 x,y,z좌표 / 터널비용은 min(ads(x-x), y-y, z-z) / 모든행성 연결 최소비용
-# 크루스칼
+# 크루스칼 #🌟시간초과!!
 
 def find_parent(parent, x):
     if parent[x] != x:
@@ -18,22 +18,31 @@ N = int(input())
 parent = [0]*N
 for i in range(N):
     parent[i] = i
-points = []
+# points = []   #전체 다 받아서 한번에 계산하면 시간초과. x,y,z 따로 비교해서 3번*N하면 최적!
 edges = []
 result = 0
 
-for _ in range(N):
-    x,y,z = map(int, input().split())
-    points.append((x,y,z))
+x = []
+y = []
+z = []
+
+for i in range(N):
+    X, Y, Z = map(int, input().split()) #변수다르게 해야함..!
+    x.append((X, i)) #노드의 번호 꼭 같이 저장해놓기!
+    y.append((Y, i))
+    z.append((Z, i))
+
+x.sort() #이때 sort할때는 절댓값x... 노드끼리의 가까운 정도대로 정렬하기!
+y.sort()
+z.sort()
 
 for i in range(N-1):
-    for j in range(i+1, N):
-        x1,y1,z1 = points[i]
-        x2,y2,z2 = points[j]
-        cost = min(abs(x1-x2), abs(y1-y2), abs(z1-z2))
-        edges.append((cost, i, j))
+    edges.append((x[i+1][0]-x[i][0], x[i][1],  x[i+1][1] )) #이미 위에서 sort했기 때문에 더큰애-작은애 abs()쓴것처럼 됨-->cost
+                                    #x[i][1] : a노드의 번호  /  x[i+1][1] : b노드의 번호
+    edges.append((y[i+1][0]-y[i][0], y[i][1],  y[i+1][1] ))
+    edges.append((z[i+1][0]-z[i][0], z[i][1],  z[i+1][1] ))
     
-edges.sort()
+edges.sort() #그럼 알아서 abs(x1-x2), abs(y1-y2), abs(z1-z2) 중에 min가장 작은거 들어감..ㄷㄷ..
 
 for e in edges:
     cost, a, b = e
